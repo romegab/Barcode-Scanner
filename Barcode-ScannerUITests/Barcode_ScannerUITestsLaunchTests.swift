e1d54151d09bee4421eba10563bb129b767676c4
romegab <idstoilov@gmial.com>
2024-02-04 17:34:44 +0200

    Initial commit

---
+++ Barcode-ScannerUITests/Barcode_ScannerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Barcode_ScannerUITestsLaunchTests.swift
+//  Barcode-ScannerUITests
+//
+//  Created by Ivan Stoilov on 4.02.24.
+//
+
+import XCTest
+
+final class Barcode_ScannerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
